98bdbbad3cb44269fd9e646cc1447b06f24af9d1
Xuan Thanh <lxthanh235@gmail.com>
2023-11-12 00:30:44 +0700

    8. Validating Input via Built-in Validation Props

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,106 +1,101 @@
 export default function Signup() {
-  function handleSubmit(event) {
-    event.preventDefault();
-    const fd = new FormData(event.target);
-    const acquisition = fd.getAll("acquisition");
-    const data = Object.fromEntries(fd.entries());
-    data.acquisition = acquisition;
-    console.log(data);
-  }
-  return (
-    <form onSubmit={handleSubmit}>
-      <h2>Welcome on board!</h2>
-      <p>We just need a little bit of data from you to get you started 🚀</p>
+ function handleSubmit(event) {
+  event.preventDefault();
+  const fd = new FormData(event.target);
+  const acquisition = fd.getAll("acquisition");
+  const data = Object.fromEntries(fd.entries());
+  data.acquisition = acquisition;
+  console.log(data);
 
-      <div className="control">
-        <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
-      </div>
+  //8. Validating Input via Built-in Validation Props
+  // thêm required và type
+ }
+ return (
+  <form onSubmit={handleSubmit}>
+   <h2>Welcome on board!</h2>
+   <p>We just need a little bit of data from you to get you started 🚀</p>
 
-      <div className="control-row">
-        <div className="control">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
-        </div>
+   <div className="control">
+    <label htmlFor="email">Email</label>
+    <input id="email" type="email" name="email" required />
+   </div>
 
-        <div className="control">
-          <label htmlFor="confirm-password">Confirm Password</label>
-          <input
-            id="confirm-password"
-            type="password"
-            name="confirm-password"
-          />
-        </div>
-      </div>
+   <div className="control-row">
+    <div className="control">
+     <label htmlFor="password">Password</label>
+     <input
+      id="password"
+      type="password"
+      name="password"
+      required
+      minLength={6}
+     />
+    </div>
 
-      <hr />
+    <div className="control">
+     <label htmlFor="confirm-password">Confirm Password</label>
+     <input id="confirm-password" type="password" name="confirm-password" />
+    </div>
+   </div>
 
-      <div className="control-row">
-        <div className="control">
-          <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
-        </div>
+   <hr />
 
-        <div className="control">
-          <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
-        </div>
-      </div>
+   <div className="control-row">
+    <div className="control">
+     <label htmlFor="first-name">First Name</label>
+     <input type="text" id="first-name" name="first-name" />
+    </div>
 
-      <div className="control">
-        <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
-          <option value="student">Student</option>
-          <option value="teacher">Teacher</option>
-          <option value="employee">Employee</option>
-          <option value="founder">Founder</option>
-          <option value="other">Other</option>
-        </select>
-      </div>
+    <div className="control">
+     <label htmlFor="last-name">Last Name</label>
+     <input type="text" id="last-name" name="last-name" />
+    </div>
+   </div>
 
-      <fieldset>
-        <legend>How did you find us?</legend>
-        <div className="control">
-          <input
-            type="checkbox"
-            id="google"
-            name="acquisition"
-            value="google"
-          />
-          <label htmlFor="google">Google</label>
-        </div>
+   <div className="control">
+    <label htmlFor="phone">What best describes your role?</label>
+    <select id="role" name="role" required>
+     <option value="student">Student</option>
+     <option value="teacher">Teacher</option>
+     <option value="employee">Employee</option>
+     <option value="founder">Founder</option>
+     <option value="other">Other</option>
+    </select>
+   </div>
 
-        <div className="control">
-          <input
-            type="checkbox"
-            id="friend"
-            name="acquisition"
-            value="friend"
-          />
-          <label htmlFor="friend">Referred by friend</label>
-        </div>
+   <fieldset>
+    <legend>How did you find us?</legend>
+    <div className="control">
+     <input type="checkbox" id="google" name="acquisition" value="google" />
+     <label htmlFor="google">Google</label>
+    </div>
 
-        <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
-          <label htmlFor="other">Other</label>
-        </div>
-      </fieldset>
+    <div className="control">
+     <input type="checkbox" id="friend" name="acquisition" value="friend" />
+     <label htmlFor="friend">Referred by friend</label>
+    </div>
 
-      <div className="control">
-        <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
-          agree to the terms and conditions
-        </label>
-      </div>
+    <div className="control">
+     <input type="checkbox" id="other" name="acquisition" value="other" />
+     <label htmlFor="other">Other</label>
+    </div>
+   </fieldset>
 
-      <p className="form-actions">
-        <button type="reset" className="button button-flat">
-          Reset
-        </button>
-        <button type="submit" className="button">
-          Sign up
-        </button>
-      </p>
-    </form>
-  );
+   <div className="control">
+    <label htmlFor="terms-and-conditions">
+     <input type="checkbox" id="terms-and-conditions" name="terms" />I agree to
+     the terms and conditions
+    </label>
+   </div>
+
+   <p className="form-actions">
+    <button type="reset" className="button button-flat">
+     Reset
+    </button>
+    <button type="submit" className="button">
+     Sign up
+    </button>
+   </p>
+  </form>
+ );
 }
