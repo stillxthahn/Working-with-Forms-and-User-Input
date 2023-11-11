ff30f105b0a38195f15d110405356c01f535827f
stillxthahn <lxthanh235@gmail.com>
2023-11-11 19:56:05 +0700

    !!!!3. Getting Values via FormData & Native Browser APIs

---
+++ src/components/Signup.jsx
@@ -0,0 +1,124 @@
+export default function Signup() {
+  //3. Getting Values via FormData & Native Browser APIs
+
+  // because you can of course use state and revs
+  // but setting up and handling all those different state values
+  // or all those different revs can of course be a lot of work.
+  // And that's why you should also at least consider
+  // using a native built-in feature
+  // for getting hold of all those values
+  // because it turns out that the browser actually helps you
+  // with handling that form submission
+  // and getting hold of all the entered values.
+
+  //Phải có prop NAME cho các input kể cả select
+  function handleSubmit(event) {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    //const enteredEmail = fd.get("email");
+    //const enteredPassword = fd.get("password");
+    //Tham số của get là thuộc tính NAME
+    //Nhưng mà làm vậy thì lâu -> Dùng built-in OBJECT class
+    //Có thể bị mất Entries -> phải dùng Extracing & Storing
+    const acquisition = fd.getAll("acquisition");
+    const data = Object.fromEntries(fd.entries());
+    data.acquisition = acquisition;
+    console.log(data);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Welcome on board!</h2>
+      <p>We just need a little bit of data from you to get you started 🚀</p>
+
+      <div className="control">
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" name="email" />
+      </div>
+
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" />
+        </div>
+
+        <div className="control">
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            id="confirm-password"
+            type="password"
+            name="confirm-password"
+          />
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" name="first-name" />
+        </div>
+
+        <div className="control">
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" name="last-name" />
+        </div>
+      </div>
+
+      <div className="control">
+        <label htmlFor="phone">What best describes your role?</label>
+        <select id="role" name="role">
+          <option value="student">Student</option>
+          <option value="teacher">Teacher</option>
+          <option value="employee">Employee</option>
+          <option value="founder">Founder</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+
+      <fieldset>
+        <legend>How did you find us?</legend>
+        <div className="control">
+          <input
+            type="checkbox"
+            id="google"
+            name="acquisition"
+            value="google"
+          />
+          <label htmlFor="google">Google</label>
+        </div>
+
+        <div className="control">
+          <input
+            type="checkbox"
+            id="friend"
+            name="acquisition"
+            value="friend"
+          />
+          <label htmlFor="friend">Referred by friend</label>
+        </div>
+
+        <div className="control">
+          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <label htmlFor="other">Other</label>
+        </div>
+      </fieldset>
+
+      <div className="control">
+        <label htmlFor="terms-and-conditions">
+          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          agree to the terms and conditions
+        </label>
+      </div>
+
+      <p className="form-actions">
+        <button type="reset" className="button button-flat">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Sign up
+        </button>
+      </p>
+    </form>
+  );
+}
