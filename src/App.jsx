15145c330f758dffe3a7d7a21b78add69d1cf163
Xuan Thanh <lxthanh235@gmail.com>
2023-11-12 00:24:06 +0700

    7. Validating Input Upon Form Submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header.jsx";
-import StateLogin from "./components/StateLogin.jsx";
+import Login from "./components/Login.jsx";
 function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <StateLogin />
-      </main>
-    </>
-  );
+ return (
+  <>
+   <Header />
+   <main>
+    <Login />
+   </main>
+  </>
+ );
 }
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,40 +1,49 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
-  //2. Getting User Input via Refs
-  const email = useRef();
-  const password = useRef();
-  function handleSubmit(event) {
-    event.preventDefault();
-    const enteredEmail = email.current.value;
-    const enteredPassword = password.current.value;
-    console.log(enteredEmail, enteredPassword);
-    //4. Resetting Form
-    //không nên dùng do ảnh hưởng tới DOM
-    email.current.value = "";
-    password.current.value = "";
+ const [emailIsInValid, setEmailIsInValid] = useState(false);
+ //2. Getting User Input via Refs
+ const email = useRef();
+ const password = useRef();
+ function handleSubmit(event) {
+  event.preventDefault();
+  const enteredEmail = email.current.value;
+  const enteredPassword = password.current.value;
+
+  //7. Validating Input Upon Form Submission
+  const emailIsValid = enteredEmail.includes("@");
+
+  if (!emailIsValid) {
+   setEmailIsInValid(true);
+   return;
   }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
+  setEmailIsInValid(false);
+ }
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input ref={email} id="email" type="email" name="email" />
-        </div>
+ return (
+  <form onSubmit={handleSubmit}>
+   <h2>Login</h2>
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input ref={password} id="password" type="password" name="password" />
-        </div>
-      </div>
+   <div className="control-row">
+    <div className="control no-margin">
+     <label htmlFor="email">Email</label>
+     <input ref={email} id="email" type="email" name="email" />
+     <div className="control-error">
+      {emailIsInValid && <p>Please enter a valid email address!</p>}
+     </div>
+    </div>
 
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
-      </p>
-    </form>
-  );
+    <div className="control no-margin">
+     <label htmlFor="password">Password</label>
+     <input ref={password} id="password" type="password" name="password" />
+    </div>
+   </div>
+
+   <p className="form-actions">
+    <button className="button button-flat">Reset</button>
+    <button className="button">Login</button>
+   </p>
+  </form>
+ );
 }
