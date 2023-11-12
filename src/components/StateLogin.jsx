c232c7a24df9a7366a374077766dcda3a477b944
Xuan Thanh <lxthanh235@gmail.com>
2023-11-13 01:31:49 +0700

    !!!11. Creating a Custom useInput Hook

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,48 +1,30 @@
 import { useState } from "react";
 import Input from "./Input";
 import { isEmail, isNotEmpty, hasMinLength } from "../util/validation";
+import { useInput } from "../hooks/useInput";
 export default function Login() {
  //1. Managing & Getting User Input via State & Generic Handlers
+ const {
+  value: emailValue,
+  handleInputChange: handleEmailChange,
+  handleInputBlur: handleEmailBlur,
+  hasError: emailHasError,
+ } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
 
- const [enteredValues, setEnteredValues] = useState({
-  email: "",
-  password: "",
- });
-
- const [didEdit, setDidEdit] = useState({
-  email: false,
-  password: false,
- });
-
- // And we can check whether emailIsInvalid
-
- // by not checking for whether it still has the initial value
-
- // but by instead keeping this check and combining this check
-
- // with didEdit.email.
-
- // If the user did edit the email address
-
- // and the email then does not include an @ symbol,
-
- // I want to set emailIsInvalid as true
- const emailIsInValid =
-  didEdit.email &&
-  !isEmail(enteredValues.email) &&
-  !isNotEmpty(enteredValues.email);
- const passwordIsInValid =
-  didEdit.password && !hasMinLength(enteredValues.password, 6);
+ const {
+  value: passwordValue,
+  handleInputChange: handlePasswordChange,
+  handleInputBlur: handlePasswordBlur,
+  hasError: passwordHasError,
+ } = useInput("", (value) => hasMinLength(value, 6));
 
  // !!! BLUR RA NGOÀI VÀ TRONG INPUT KHÔNG CÓ @
  function handleSubmit(event) {
   event.preventDefault();
-  console.log(enteredValues);
-  //4. Resetting Form
-  setEnteredValues({
-   email: "",
-   password: "",
-  });
+  if (emailHasError || passwordHasError) {
+   return;
+  }
+  console.log(emailValue, passwordValue);
  }
 
  // And indeed, combining validation on every keystroke
@@ -54,52 +36,33 @@ export default function Login() {
  // whenever the user starts typing again
 
  // can be a good pattern for validating user input.
- function handleInputChange(identifier, value) {
-  setEnteredValues((prevValues) => ({
-   ...prevValues,
-   [identifier]: value,
-  }));
-  setDidEdit((prevEdit) => ({
-   ...prevEdit,
-   [identifier]: false,
-  }));
- }
+ // 10. Building & Using a Reusable Input Component
 
- //6. Validating Input Upon Lost Focus (Blur)
- function handleInputBlur(identifier) {
-  setDidEdit((prevEdit) => ({
-   ...prevEdit,
-   [identifier]: true,
-  }));
- }
  return (
   <form onSubmit={handleSubmit}>
    <h2>Login</h2>
-
    <div className="control-row">
-    //10. Building & Using a Reusable Input Component
     <Input
      label="Email"
      id="email"
      type="email"
      name="email"
-     onChange={(event) => handleInputChange("email", event.target.value)}
-     onBlur={() => handleInputBlur("email")}
-     value={enteredValues.email}
-     error={emailIsInValid && "Please enter a valid email!"}
+     onChange={handleEmailChange}
+     onBlur={handleEmailBlur}
+     value={emailValue}
+     error={emailHasError && "Please enter a valid email!"}
     />
     <Input
      label="Password"
      id="password"
      type="password"
      name="password"
-     onChange={(event) => handleInputChange("password", event.target.value)}
-     onBlur={() => handleInputBlur("password")}
-     value={enteredValues.password}
-     error={passwordIsInValid && "Please enter a valid password!"}
+     onChange={handlePasswordChange}
+     onBlur={handlePasswordBlur}
+     value={passwordValue}
+     error={passwordHasError && "Please enter a valid password!"}
     />
    </div>
-
    <p className="form-actions">
     <button className="button button-flat">Reset</button>
     <button className="button">Login</button>
